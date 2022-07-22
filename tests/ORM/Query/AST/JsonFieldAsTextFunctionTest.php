c3d5d9c4d35bd4edd747580eb126ec145d532d27
j-schumann <114239+j-schumann@users.noreply.github.com>
2022-07-22 12:28:01 +0200

    add: test for JSON_FIELD_AS_TEXT

---
+++ tests/ORM/Query/AST/JsonFieldAsTextFunctionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vrok\DoctrineAddons\Tests\ORM\Query\AST;
+
+use Vrok\DoctrineAddons\ORM\Query\AST\JsonContainsTextFunction;
+use Vrok\DoctrineAddons\ORM\Query\AST\JsonFieldAsTextFunction;
+use Vrok\DoctrineAddons\Tests\ORM\AbstractOrmTestCase;
+
+class JsonFieldAsTextFunctionTest extends AbstractOrmTestCase
+{
+    public function testFunction()
+    {
+        $this->configuration->addCustomStringFunction('JSON_AS_TEXT', JsonFieldAsTextFunction::class);
+
+        $query = $this->buildEntityManager()->createQuery("SELECT t.id FROM Vrok\DoctrineAddons\Tests\Fixtures\TestEntity t WHERE JSON_AS_TEXT(t.jsonColumn, 'city') = 'Dresden'");
+        $this->assertEquals("SELECT t0_.id AS id_0 FROM TestEntity t0_ WHERE (t0_.jsonColumn->>'city') = 'Dresden'", $query->getSQL());
+    }
+}
