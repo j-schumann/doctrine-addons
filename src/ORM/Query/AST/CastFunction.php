731bd02467f8138db584d43da736d8b75cc072e3
Jakob Schumann <114239+j-schumann@users.noreply.github.com>
2024-09-25 19:22:14 +0200

    upd: phpunit upd: ORM 3 compatibility

--- src/ORM/Query/AST/CastFunction.php
+++ src/ORM/Query/AST/CastFunction.php
@@ -7,9 +7,9 @@ namespace Vrok\DoctrineAddons\ORM\Query\AST;
 use Doctrine\ORM\Query\AST\ArithmeticExpression;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\AST\Node;
-use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
 
 /**
  * @see https://gist.github.com/galiazzi/5e5f04f9753ba4d8a9b972c87dc2a805
@@ -23,12 +23,12 @@ class CastFunction extends FunctionNode
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
         $this->expr1 = $parser->ArithmeticExpression();
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
         $this->expr2 = $parser->StringPrimary();
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker): string
